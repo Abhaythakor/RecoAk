f2a653dd616e7a00a11da57b925456533f12760d
Abhaythakor <95174477+Abhaythakor@users.noreply.github.com>
2023-05-06 13:15:58 +0530

    Update amass.go

--- subdomain/amass.go
+++ subdomain/amass.go
@@ -7,8 +7,10 @@ import (
     "os/exec"
     "strings"
     "time"
-
+    "bufio"
+    "bytes"
     "github.com/schollz/progressbar"
+    "io"
 )
 
 func amassEnum(path string) {
@@ -58,38 +60,4 @@ func amassEnum(path string) {
             }
         }
     }
-}
-func amassEnum(path string) {
-    t := time.Now()
-    Hour := t.Format("15")
-    Minute := t.Format("04")
-    Second := t.Format("05")
-    subdomainFile := fmt.Sprintf("%s/Subdomain/subdomain.txt", path)
-    outputDir := fmt.Sprintf("%s/Subdomain/amassIp", path)
-
-    // Count the number of lines in the subdomain file
-    subdomainFileHandle, err := os.Open(subdomainFile)
-    if err != nil {
-        log.Fatalf("Error opening subdomain file: %v", err)
-    }
-    defer subdomainFileHandle.Close()
-    subdomainScanner := bufio.NewScanner(subdomainFileHandle)
-    lineCount := 0
-    for subdomainScanner.Scan() {
-        lineCount++
-    }
-
-    // Create progress bar
-    bar := progressbar.Default(int64(lineCount))
-
-    // Run amass command
-    cmd := exec.Command("amass", "enum", "-ip", "-df", subdomainFile, "-dir", outputDir, "-active", "-config", "/root/config.ini")
-    var out bytes.Buffer
-    cmd.Stdout = io.MultiWriter(&out, bar) // Use MultiWriter to write to both the buffer and the progress bar
-    err = cmd.Run()
-    if err != nil {
-        log.Fatalf("Error running amass: %v", err)
-    }
-
-    fmt.Printf("Amass complete at (\033[34m%d:%d:%d\033[0m)\n", Hour, Minute, Second)
 }
